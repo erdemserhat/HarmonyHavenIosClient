35c7e8886d543ec460bb18163e5a2a74b5e6d827
serhaterdem <serhaterdem961@gmail.com>
2025-02-27 20:53:19 +0300

    Initial Commit

---
+++ Harmony Haven/Harmony_HavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harmony_HavenApp.swift
+//  Harmony Haven
+//
+//  Created by Serhat Erdem on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Harmony_HavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Harmony HavenTests/Harmony_HavenTests.swift
@@ -0,0 +1,17 @@
+//
+//  Harmony_HavenTests.swift
+//  Harmony HavenTests
+//
+//  Created by Serhat Erdem on 27.02.2025.
+//
+
+import Testing
+@testable import Harmony_Haven
+
+struct Harmony_HavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
